f419cf43d374f28ef0bc6c8b16c46433aec5bfa2
Anamarkelova <amarkelova@myyahoo.com>
2026-07-15 20:54:07 -0400

    Added favicon and updated Basic CSS project

---
+++ HelloWorld/Hello_World.js
@@ -0,0 +1,8 @@
+let greeting = "Hello, World!";
+console.log(greeting);
+
+var greetind2 = 'Welcome to Node.js';
+console.log(greetind2);
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentence1);
